158efd6039603fece95e40a8c9f472a683ca2ed5
akhrorwork <akhrorwork@gmail.com>
2024-02-08 16:21:49 +0500

    recepi project ready

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+      serif: ["Young Serif", "serif"],
+    },
+    extend: {
+      backgroundColor: {
+        main_pink: "#F3E5D7",
+      },
+      colors: {
+        "wenge-brown": "#5F564D",
+        snow: "#FFF7FB",
+        "dark-resbarry": "#7A284E",
+      },
+    },
+  },
+  plugins: [],
+};
